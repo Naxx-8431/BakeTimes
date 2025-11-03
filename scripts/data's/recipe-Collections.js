aa8847f3233efbc9c184a855cc464da4511d4dc2
Naxx-8431 <25133@yenepoya.edu.in>
2025-11-03 20:56:39 +0530

    recipe page maid interactive

---
+++ scripts/data's/recipe-Collections.js
@@ -0,0 +1,25 @@
+const recipeList = [{
+
+  name: 'Vanilla Sponge Cake',
+  image: 'tumbnail-images/recipe-Images/recipe-Vanilla-Sponge-Cake.png', 
+  category: 'Cake',
+  featured: true
+}, {
+
+  name: 'Apple Scones',
+  image: 'tumbnail-images/recipe-Images/recipe-Apple-Scones.png',
+  category: 'Pastry',
+  featured: true
+}, {
+
+  name: 'Classic White Bread',
+  image: 'tumbnail-images/recipe-Images/recipe-Classic-White-Bread.png',
+  category: 'Bread',
+  featured: true
+}, {
+
+  name: 'Chocolate Fudge Cake',
+  image: 'tumbnail-images/recipe-Images/recipe-Chocolate-Fudge-Cake.png',
+  category: 'Cake',
+  featured: true
+}];
---
+++ scripts/recipe.js
@@ -0,0 +1,84 @@
+function renderFeaturedRecipes(recipes = recipeList) {
+
+
+  const featuredRecipes = recipes.filter(recipe => recipe.featured === true);
+
+  let featuredRecipeHTML = '';
+
+  featuredRecipes.forEach((recipe) => {
+    const featuredHTML = `
+    <div class="recipe-card">
+        <a href=""><img src="${recipe.image}" alt="${recipe.name}" class="recipe-image"></a>
+        <div class="recipe-title">
+            <p>${recipe.name}</p>
+        </div>
+    </div>
+  `;
+
+  featuredRecipeHTML += featuredHTML;
+  });
+
+  document.querySelector('.js-Featured-Recipes').innerHTML = featuredRecipeHTML
+
+};
+
+renderFeaturedRecipes();
+
+
+
+
+function recipeListHTML(recipes = recipeList) {
+  
+  let recipeHTML = '';
+
+  recipes.forEach((recipe) => {
+    const html = `
+        <div class="recipe-card">
+          <a href=""><img src="${recipe.image}" alt="${recipe.name}" class="recipe-image"></a>
+          <div class="recipe-title">
+              <p>${recipe.name}</p>
+          </div>
+        </div>
+      `;
+
+      recipeHTML += html;
+  });
+
+  document.querySelector('.js-recipes').innerHTML = recipeHTML;
+
+};
+
+recipeListHTML();
+
+
+
+const featuredRecipes = document.getElementById('featured-Section');
+const featuredRecipesText = document.querySelector('.js-recipeText');
+const allRecipesText = document.querySelector('.js-allRecipeText');
+const Line = document.querySelector('.js-line');
+
+
+
+document.getElementById('bar').addEventListener('input', (event) => {
+
+  const searchText = event.target.value.toLowerCase();
+
+
+  const filteredRecipes = recipeList.filter(recipe => recipe.name.toLowerCase().includes(searchText));
+
+
+if(searchText.trim() !== '') {
+  featuredRecipes.style.display = 'none';
+  featuredRecipesText.style.display = 'none';
+  allRecipesText.style.display = 'none';
+  Line.style.display = 'none';
+} else {
+  featuredRecipes.style.display = 'flex';
+  featuredRecipesText.style.display = 'block';
+  allRecipesText.style.display = 'block';
+  Line.style.display = 'block';
+}
+
+  recipeListHTML(filteredRecipes);
+
+});
